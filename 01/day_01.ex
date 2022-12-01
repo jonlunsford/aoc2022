45babe0a7c9bf0af0c66e7d8829d6a3c386a2c0c
Jon Lunsford <jon@capturethecastle.net>
2022-11-30 21:42:47 -0800

    Day 01

--- 01/day_01.ex
+++ 01/day_01.ex
@@ -1,6 +1,38 @@
 defmodule Day01 do
-  def hello_aoc do
-    :hello_aoc
+  def part_1(file_name) do
+    file_name
+    |> parse_file()
+    |> Enum.to_list()
+    |> Enum.max()
+  end
+
+  def part_2(file_name) do
+    file_name
+    |> parse_file()
+    |> Enum.to_list()
+    |> Enum.sort()
+    |> Enum.reverse()
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
+
+  defp parse_file(file_name) do
+    file_name
+    |> File.stream!()
+    |> Stream.chunk_by(fn line -> line == "\n" end)
+    |> Stream.reject(fn [head | _] -> head == "\n" end)
+    |> Stream.map(&sum_list/1)
+  end
+
+  defp sum_list(list) do
+    list
+    |> Enum.map(fn int ->
+      case Integer.parse(int) do
+        :error-> 0
+        {int, _} -> int
+      end
+    end)
+    |> Enum.sum()
   end
 end
 
@@ -9,7 +41,11 @@ ExUnit.start()
 defmodule Day01Test do
   use ExUnit.Case
 
-  test "it works" do
-    assert :hello_aoc = Day01.hello_aoc()
+  test "part 1 does the thing correctly" do
+    assert Day01.part_1("input-test.txt") == 24_000
+  end
+
+  test "part 2 also sort of maybe does the thing correctly" do
+    assert Day01.part_2("input-test.txt") == 45_000
   end
 end
